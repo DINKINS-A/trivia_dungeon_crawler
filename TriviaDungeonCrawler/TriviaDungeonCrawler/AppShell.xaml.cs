56db79da36a6a0ebf3a8101240fef64f0df01b3f
Ahmad Dinkins <ahmaddinkins@student.purdueglobal.edu>
2025-07-03 09:03:48 -0400

    SCRUM-25 Finished creating project skeleton using the basic template.

---
+++ TriviaDungeonCrawler/TriviaDungeonCrawler/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TriviaDungeonCrawler
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TriviaDungeonCrawler/TriviaDungeonCrawler/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TriviaDungeonCrawler
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
