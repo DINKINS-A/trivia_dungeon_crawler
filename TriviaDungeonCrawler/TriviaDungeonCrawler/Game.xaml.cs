e55e847e1cc1b10139d8adeeb81ab42e62ceee62
Ahmad Dinkins <ahmaddinkins@student.purdueglobal.com>
2025-07-04 10:53:35 -0400

    SCRUM-28 Display maze on gameplay screen.

--- TriviaDungeonCrawler/TriviaDungeonCrawler/Game.xaml.cs
+++ TriviaDungeonCrawler/TriviaDungeonCrawler/Game.xaml.cs
@@ -1,19 +1,27 @@
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Windows.Input;
 
 namespace TriviaDungeonCrawler;
 
 public partial class Game : ContentPage
 {
+    private const int MAZE_HEIGHT = 10;
+    private const int MAZE_WIDTH = 10;
+
     private ImageButton? up;
     private ImageButton? down;
     private ImageButton? left;
     private ImageButton? right;
 
+    private List<Cell> maze = [];
+
 	public Game()
 	{
 		InitializeComponent();
+        CreateMaze();
 		CreateLevel();
 	}
 
@@ -74,13 +82,14 @@ public partial class Game : ContentPage
             grid.AddColumnDefinition(column);
         }
 
-        for(int i = 0; i < 10; i++)
+        for(int i = 0; i < MAZE_HEIGHT; i++)
         {
-            for (int j = 0; j < 10; j++)
+            for (int j = 0; j < MAZE_WIDTH; j++)
             {
+                string pathToImage = GetWallImage(i, j);
                 grid.Add(new Image
                 {
-                    Source = "wall_all.png"
+                    Source = pathToImage
                 }, i, j);
             }
         }
@@ -90,6 +99,74 @@ public partial class Game : ContentPage
 
         return grid;
 	}
+
+    /*
+     * Returns path to image correspoing to the number of walls the cell at given corrdinate has
+     */
+    private string GetWallImage(int x, int y)
+    {
+        Cell cell = maze[x * MAZE_HEIGHT + y];
+
+        // Check which walls have been torn down and return corresponding path to image
+        if (cell.north_wall && cell.east_wall && cell.south_wall)
+        {
+            return "wall_nse.png";
+        }
+        else if (cell.north_wall && cell.south_wall && cell.west_wall) 
+        {
+            return "wall_nsw.png";
+        }
+        else if (cell.north_wall && cell.east_wall && cell.west_wall) 
+        {
+            return "wall_new.png";
+        }
+        else if (cell.south_wall && cell.east_wall && cell.west_wall) 
+        {
+            return "wall_sew.png";
+        }
+        else if (cell.north_wall && cell.south_wall)
+        {
+            return "wall_ns.png";
+        }
+        else if (cell.north_wall && cell.east_wall)
+        {
+            return "wall_ne.png";
+        }
+        else if (cell.north_wall && cell.west_wall)
+        {
+            return "wall_nw.png";
+        }
+        else if (cell.south_wall && cell.east_wall)
+        {
+            return "wall_se.png";
+        }
+        else if (cell.south_wall && cell.west_wall)
+        {
+            return "wall_sw.png";
+        }
+        else if (cell.east_wall && cell.west_wall)
+        {
+            return "wall_ew.png";
+        }
+        else if (cell.north_wall)
+        {
+            return "wall_n.png";
+        }
+        else if (cell.south_wall)
+        {
+            return "wall_s.png";
+        }
+        else if (cell.east_wall)
+        {
+            return "wall_e.png";
+        }
+        else if (cell.west_wall)
+        {
+            return "wall_w.png";
+        }
+
+        return "wall_none.png";
+    }
 
     /* 
      * Create controls for player movement
@@ -141,5 +218,115 @@ public partial class Game : ContentPage
     {
 
     }
+
+    private void InitializeMaze()
+    {
+        for (int i = 0; i < MAZE_HEIGHT * MAZE_WIDTH; i++)
+        {
+            maze.Add(new Cell(i / MAZE_HEIGHT, i % MAZE_WIDTH));
+        }
+    }
+    
+    private void CreateMaze()
+    {
+        InitializeMaze();
+        PopulateNeighbors();
+
+        // Choose starting cell
+        Random rand = new();
+        int randX = rand.Next(MAZE_HEIGHT);
+        int randY = rand.Next(MAZE_HEIGHT);
+        RunDepthFirstSearch(maze[randX * MAZE_HEIGHT + randY]);
+    }
+
+    private void RunDepthFirstSearch(Cell cell)
+    {
+        cell.visited = true;
+        while (HasUnvisitedNeighbor(cell))
+        {
+            Cell neighbor = GetRandomNeighbor(cell);
+            RunDepthFirstSearch(neighbor);
+        }
+    }
+
+    /* 
+     * Returns true if this cell has a neighbor that has not been visited
+     */
+    private static bool HasUnvisitedNeighbor(Cell cell) => cell.neighbors.FindAll(neighbor => !neighbor.visited).Count() > 0;
+
+    /*
+     * Add neighbors to each cell
+     */
+    private void PopulateNeighbors()
+    {
+        for (int i = 0; i < MAZE_HEIGHT; i++)
+        {
+            for (int j = 0; j < MAZE_WIDTH; j++)
+            {
+                if (j > 0)
+                {
+                    // left neighbor
+                    maze[i * MAZE_HEIGHT + j].neighbors.Add(maze[i * MAZE_HEIGHT + (j - 1)]);
+                }
+                if (j < MAZE_HEIGHT - 1)
+                {
+                    // right neighbor
+                    maze[i * MAZE_HEIGHT + j].neighbors.Add(maze[i * MAZE_HEIGHT + (j + 1)]);
+
+                }
+                if (i > 0)
+                {
+                    // top neighbor
+                    maze[i * MAZE_HEIGHT + j].neighbors.Add(maze[(i - 1) * MAZE_HEIGHT + j]);
+                }
+                if (i < MAZE_WIDTH - 1)
+                {
+                    // bottom neighbor
+                    maze[i * MAZE_HEIGHT + j].neighbors.Add(maze[(i + 1) * MAZE_HEIGHT + j]);
+                }
+            }
+        }
+    }
+
+    private Cell GetRandomNeighbor(Cell cell)
+    {
+        Random r = new();
+        List<Cell> neighbors = cell.neighbors.FindAll(neighbor => !neighbor.visited);
+        Cell chosenNeighbor = neighbors[r.Next(neighbors.Count())];
+        // tear down walls
+        if (chosenNeighbor.coordniate.Item1 > cell.coordniate.Item1)
+        {
+            chosenNeighbor.west_wall = false;
+            cell.east_wall = false;
+        } else if (chosenNeighbor.coordniate.Item1 < cell.coordniate.Item1) {
+            chosenNeighbor.east_wall = false;
+            cell.west_wall = false;
+        } else if (chosenNeighbor.coordniate.Item2 > cell.coordniate.Item2)
+        {
+            chosenNeighbor.north_wall = false;
+            cell.south_wall = false;
+        } else if (chosenNeighbor.coordniate.Item2 < cell.coordniate.Item2) {
+            chosenNeighbor.south_wall = false;
+            cell.north_wall = false;
+        }
+
+        return chosenNeighbor;
+    }
+
+    private class Cell
+    {
+        public Tuple<int, int> coordniate;
+        public bool north_wall = true;
+        public bool south_wall = true;
+        public bool east_wall = true;
+        public bool west_wall = true;
+        public bool visited = false;
+        public List<Cell> neighbors = new List<Cell>();
+
+        public Cell(int x, int y)
+        {
+            coordniate = new Tuple<int, int>(x, y);
+        }
+    };
 
 }
